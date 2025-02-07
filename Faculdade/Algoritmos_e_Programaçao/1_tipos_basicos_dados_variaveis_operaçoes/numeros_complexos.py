90af61b516d9e2b8e14d7f2fb35834a80f1d6f4f
Vanessa Do Santos <vanessa.pitu@gmail.com>
2025-02-07 12:57:12 -0300

    att
    Atualizando tudo.

--- Faculdade/Algoritmos_e_Programaçao/1_tipos_basicos_dados_variaveis_operaçoes/numeros_complexos.py
+++
@@ -1,7 +0,0 @@
-#Crie um programa que receba dois números complexos e mostre a soma deles.
-
-num1 = complex (input ("Digite o primeiro número complexo:"))
-num2 = complex (input ("Digite o segundo número complexo:"))
-print (f"A soma de dois números complexos é: {num1 + num2}")
-
-
--- Faculdade/Algoritmos_e_Programaçao/1_tipos_basicos_dados_variaveis_operaçoes/strings.py
+++
@@ -1,5 +0,0 @@
-#Crie um programa que receba seu nome e idade e imprima uma mensagem dizendo "Olá, [nome]! Você tem [idade] anos".
-
-nome = input ("Qual é o seu nome?")
-idade = input ("Quantos anos você tem? ")
-print (f"Olá, {nome}! Você tem {idade} anos.")
